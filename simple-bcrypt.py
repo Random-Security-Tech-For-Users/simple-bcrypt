d849ce2d40f3b87ec30c32e88a2622ca6e0e7094
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-11 14:30:49 -0400

    Update simple-bcrypt.py

--- simple-bcrypt.py
+++ simple-bcrypt.py
@@ -1,1 +1,15 @@
-import bcrypt
+
+def bhash(password):
+  try:
+    import bcrypt
+    hashed = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
+    return hashed
+  except:
+    return None
+ def verify(password,hashed):
+  try:
+    import bcrypt
+    return bcrypt.checkpw(password.encode(),hashed)
+  except:
+    return None
+ #END

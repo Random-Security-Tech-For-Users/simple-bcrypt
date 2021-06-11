c421bd7a159503b914513ce48124eb065ef78a5a
iam-py-test <axbina@outlook.com>
2021-06-11 14:47:55 -0400

    Updates

--- simple-bcrypt.py
+++ simple-bcrypt.py
@@ -1,21 +1,21 @@
-
 def bhash(password):
-  if len(password) > 72:
-    raise Exception("Password above max length 72")
-  try:
-    import bcrypt
-    hashed = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
-    return hashed
-  except:
-    return None
- def verify(password,hashed):
-  try:
-    import bcrypt
-    return bcrypt.checkpw(password.encode(),hashed)
-  except:
-    return None
+    if len(password) > 72:
+        raise Exception("Password above max length 72")
+    try:
+        import bcrypt
+        hashed = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
+        return hashed.decode()
+    except:
+        return None
+def verify(password,hashed):
+    try:
+        import bcrypt
+        return bcrypt.checkpw(password.encode(),hashed.encode())
+    except:
+        return None
   
- if __name__ == '__main__':
-  passwd = input("Enter a password to test:")
-  print(bhash(passwd))
- #END
+if __name__ == '__main__':
+    passwd = input("Enter a password to test:")
+    print(bhash(passwd))
+    print(verify(passwd,bhash(passwd)))
+#END

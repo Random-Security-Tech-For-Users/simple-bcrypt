0d5fa70fa192940d3986261ee8fc2bf7c9560106
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-25 11:40:02 -0400

    Test another password

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ hash1 = simplebcrypt.bhash("password")
 testhash = simplebcrypt.bhash("password".encode())
 if simplebcrypt.verify("password",hash1) != True:
   raise Exception("Verification error")
+if simplebcrypt.verify("fakepassword",hash1) != False:
+  raise Exception("Allowed another password to be accepted")
